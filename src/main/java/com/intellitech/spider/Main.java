0b4852c71f6091fe80de5be2c0003a68bae17240
llh <llh@llhdeMacBook-Pro.local>
2015-10-29 23:30:42 +0800

    init

---
+++ src/main/java/com/intellitech/spider/Main.java
@@ -0,0 +1,10 @@
+package com.intellitech.spider;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+       System.out.println(System.getProperty("java.version"));
+        System.out.println(111);
+    }
+}
